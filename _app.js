8c92b8000a6c08c03c73708713860e04a4691054
jenniferdenton-droid <jennifer.denton@joinmoxie.com>
2026-07-10 14:38:08 -0400

    fv1

--- _app.js
+++
@@ -1,9 +0,0 @@
-import { ClerkProvider } from "@clerk/nextjs";
-
-export default function App({ Component, pageProps }) {
-  return (
-    <ClerkProvider>
-      <Component {...pageProps} />
-    </ClerkProvider>
-  );
-}
